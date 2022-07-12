27b87e63cc4496b04984ff299344e4ec2988f050
Baran Unsal <barannunsal@gmail.com>
2022-07-12 17:56:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Kelimeleri giriniz: ");
+
+foreach (var item in Console.ReadLine().Split())
+{
+    bool control = false;
+    for (int i = 1; i < item.Length; i++)
+    {
+        if (Sessiz(item[i]) == Sessiz(item[i - 1]))
+            control = true;
+    }
+    Console.Write(control + " ");
+}
+
+bool Sessiz(char x)
+{
+    return !"aeıioöuü".ToCharArray().Contains(x);
+}
